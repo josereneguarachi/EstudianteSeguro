5cb29f9ed96417913b53e15b27b40b4b9707e976
joserene123 <joseelunico2000@gmail.com>
2020-08-29 10:12:57 -0400

    Con controlador de students mas autorizacion

---
+++ ApiSeccureStudent/ApiSeccureStudent/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiSeccureStudent.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        { 
+        }
+
+        public System.Data.Entity.DbSet<ApiSeccureStudent.Models.Student> Students { get; set; }
+    }
+}
